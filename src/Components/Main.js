8f074b0d89c3a8a267c491ce36ea277dc168ed38
github username <github email address>
2020-07-25 21:20:20 -0600

    update

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,13 +1,10 @@
 import  React, {Component} from 'react';
-import List from './List'
 import Title from './Title'
 
 class Main extends Component {
   render(){
     return <div> 
-      <Title title={'toDos'} />
-      <List tasks={['Mow the lawn', 'walk the dog']}/>
-      <List tasks={['hose the driveway', 'finish the laundry']}/>
+      <Title title={'AvyGram'} />
     </div>
   } 
 }
