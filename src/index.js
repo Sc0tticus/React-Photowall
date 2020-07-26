cb4fe3bfa7b21dc828deea22b3ac19ba9f0b5c89
github username <github email address>
2020-07-25 18:36:38 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-const element = React.createElement('h1', null, 'Hello World');
 
-ReactDOM.render(element, document.getElementById('root'));
+const tasks = ['take out trash', 'shovel the driveway','walk the dog'];
+
+  // const element = 
+  //   <div>
+  //     <h1>Task List</h1>
+  //       <ol>
+  //         { tasks.map((task, index) => <li key= {index}> {task} </li>)}
+  //       </ol>
+  //   </div>
+
+class List extends Component {
+  render() {
+  return (
+    <ol>
+        {tasks.map((task, index) => <li key= {index}> {task} </li>)}
+    </ol>
+    )
+  }
+}
+
+class Title extends Component {
+  render() {
+    return <h1>Task List</h1>
+  }
+}
+
+class Main extends Component {
+  render(){
+    return <div> 
+      <Title />
+      <List />
+    </div>
+  } 
+}
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
