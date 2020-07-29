b02d885d17224504bb17d28485f7a4f9a380770a
github username <github email address>
2020-07-28 20:40:15 -0600

    Updating Front end with the ability to    add photos, comment on photos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/stylesheet.css'
+import './Styles/stylesheet.css'
 import {BrowserRouter} from 'react-router-dom'
 import {createStore} from 'redux'
 import rootReducer from './redux/reducer'
