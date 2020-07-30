751f5b5a032c98964edf8743dce168907996e449
brandon <brandon.barrett@sendgrid.com>
2020-07-29 22:43:34 -0600

    notes

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -3,17 +3,35 @@ import Photo from './Photo'
 import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
 //anchor tag, href attribute
-function PhotoWall(props) {
-return  <div> 
-             <Link className = "addIcon" to="/AddPhoto"> </Link> 
+class PhotoWall extends React.Component {
+    componentDidMount() {
+        fetch("http://localhost:3000/Posts")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                console.log('SUCCESS!!! The result:', result)
+                console.log('NOW you need to call props.addPost on the results')
+                result.forEach(element => {
+                    this.props.addPost(element);
+                })
+            },
+
+            (error) => { console.log('Shit, something went wrong!!!', error) }
+        )
+    }
+
+    render() {
+    return  <div>
+             <Link className = "addIcon" to="/AddPhoto"> </Link>
              <div className="photoGrid" >
-                  {props.posts
+                  {this.props.posts
                     .sort(function(x,y) {
                         return  y.id - x.id
                     })
-                    .map((post, index) => <Photo key={index} post={post} {...props} index={index}/>)}
+                    .map((post, index) => <Photo key={index} post={post} {...this.props} index={index}/>)}
              </div>
         </div>
+    }
 }
 
 PhotoWall.propTypes = {
